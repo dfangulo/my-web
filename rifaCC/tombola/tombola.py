7d79998a51fab3709aae3e659860e5a2ffcb100c
David Angulo <dfangulo@gmail.com>
2024-04-15 15:37:03 -0700

    Tombola con mas efectos

--- rifaCC/tombola/tombola.py
+++ rifaCC/tombola/tombola.py
@@ -37,7 +37,7 @@ class Tombola:
             print("Nombre del ganador escrito en js/json/winner.json.")
 
     def reveal_winner(self) -> None:
-        os.system('cls')
+        self.clear_screen()
         with open("js/json/winner.json") as file:
             winner_data = json.load(file)
             self.winner = winner_data.get("winner")
@@ -46,33 +46,41 @@ class Tombola:
     def animate_winner_reveal(self, participants, winner):
         # Tiempo de espera entre cada nombre (en segundos)
         wait_time = 0.5
-        self.type_text_slowly("Agradecimiento especial a todos los que nos apayoran: ")
+        header: str = f"[{"Participantes".center(30)}]" + " [Boletos]\n"
+        messaje = "\033[92mAgradecimiento especial a todos los que nos apoyan: \033[0m"
+        self.type_text_slowly(messaje)
         print()
         participants_names = [participant['name'] for  participant in participants]
         count_data_participants = Counter(participants_names)
         line: int = 0
         pages = round(len(count_data_participants)/4)
-        header: str = f"[{"Participantes".center(30)}]" + " [Boletos]\n"
+        page_header: str= messaje + '\n' + header + '\n'
+        page_text: str= ''
         print(header)
         for participant, tickets  in count_data_participants.items():
-            if line > pages:
-                os.system('cls')
+            if line > pages or line == 0:
+                time.sleep(1)
+                self.star_wars_animation(page_header, page_text)
+                page_text = ''
+                self.clear_screen()
+                print(messaje)
                 print(header)
                 line = 0
             str_names = f"[\033[92m{participant.center(30, " ")}\033[0m]" + f" [{tickets:>2} \033[91m\u2764\033[0m ]"
+            page_text += str_names.center(30) + '\n'
             self.type_text_slowly(str_names, 0.01)
             line += 1
             print()
         
         print('\n' * 2)
-        text = "¡Y el ganador es..."
+        text = "¡Y el ganador es...: "
         self.type_text_slowly(text)
         print()
         time.sleep(
             wait_time * 3
         )  # Tiempo de espera adicional antes de revelar al ganador
 
-        self.type_text_slowly(("¡" + winner["name"]+ "!"))
+        self.type_text_slowly(text=("¡" + winner["name"]+ "!").center(30), delay= 0.2)
         time.sleep(
             wait_time * 5
         )  # Tiempo de espera adicional después de revelar al ganador
@@ -84,6 +92,19 @@ class Tombola:
             time.sleep(delay)
 
 
+    def clear_screen(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def star_wars_animation(self, header ,text, speed=0.5):
+        lines = text.split('\n')
+        self.clear_screen()
+        for i in range(len(lines)):
+            self.clear_screen()
+            print(header)
+            for j in range(i+1, len(lines)):
+                print(lines[j])
+            time.sleep(speed)
+        
 if __name__ == "__main__":
     # Ejemplo de uso
     tombola = Tombola()
